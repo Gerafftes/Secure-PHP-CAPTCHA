a0767b81654ac9fcaa3411bbd034d46a14dafd83
Gerafftes <111056393+Gerafftes@users.noreply.github.com>
2025-08-08 21:20:43 +0200

    generate.php aktualisieren

--- open/generate.php
+++ open/generate.php
@@ -3,47 +3,47 @@ session_start();
 
 $step = isset($_GET['step']) ? (int)$_GET['step'] : 1;
 if (!isset($_SESSION['captcha_expected'][$step])) {
-    // Falls Step ungültig → Notfallausgabe
+    
     $char = '?';
 } else {
     $char = $_SESSION['captcha_expected'][$step];
 }
 
-/* Zielgröße für die Ausgabe */
+
 $W_final = 160; 
 $H_final = 160;
 
-/* Arbeitsfläche in kleiner Auflösung */
-$scaleFactor = 0.5; // 50% Auflösung
+
+$scaleFactor = 0.5;
 $W = (int)($W_final * $scaleFactor);
 $H = (int)($H_final * $scaleFactor);
 
-/* Output & No-Cache */
+
 header('Content-Type: image/png');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Pragma: no-cache');
 header('Expires: 0');
 
-/* Basisbild */
+
 $im = imagecreatetruecolor($W,$H);
 $bg  = imagecolorallocate($im, 205,205,205); // hellgrau
 $cg  = imagecolorallocate($im, 155,155,155); // grauer Kreis
 imagefilledrectangle($im, 0,0, $W,$H, $bg);
 imagefilledellipse($im, $W/2, $H/2, $W-4, $H-4, $cg);
 
-/* --- Rauschen --- */
+
 $noiseCount = (int)($W*$H*0.25);
 for ($i=0; $i<$noiseCount; $i++) {
     $c = imagecolorallocate($im, random_int(0,255), random_int(0,255), random_int(0,255));
     imagesetpixel($im, random_int(0,$W-1), random_int(0,$H-1), $c);
 }
 
-/* Störlinien */
-$lines = 25; // Anzahl der Linien
+
+$lines = 25;
 for ($i = 0; $i < $lines; $i++) {
     imagesetthickness($im, random_int(1, 3));
 
-    // 40% Chance auf Blauton 80,115,166 – sonst komplett zufällig
+    // 40% Chance auf Blauton 80,115,166
     if (random_int(1, 100) <= 40) {
         $lc = imagecolorallocate($im, 80, 115, 166);
     } else {
@@ -55,7 +55,7 @@ for ($i = 0; $i < $lines; $i++) {
 
 
 
-/* Text */
+
 $textColor = imagecolorallocate($im, 80, 115, 166); // RGB: Blau
 
 $angle = random_int(-18, 18);
